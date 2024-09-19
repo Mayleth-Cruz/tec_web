f94baba761e5e9bb813e3e9c87da05ca6cf2775a
May <cruzmayleth13@gmail.com>
2024-09-19 09:45:52 -0600

    Primera parte XHTML

--- practicas/p08/get_productos_xhtml.php
+++ practicas/p08/get_productos_xhtml.php
@@ -18,5 +18,23 @@ if (!empty($tope) && is_numeric($tope)) {
 
     /** Establecer el charset de la conexión */
     $link->set_charset("utf8");
+    if ($stmt = $link->prepare("SELECT * FROM productos WHERE unidades <= ?")) {
+        $stmt->bind_param("i", $tope);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        /** Generar documento XHTML */
+        echo '<?xml version="1.0" encoding="UTF-8"?>';
+        echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">';
+        echo '<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es">';
+        echo '<head>';
+        echo '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />';
+        echo '<title>Productos</title>';
+        echo '<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />';
+        echo '</head>';
+        echo '<body>';
+        echo '<div class="container">';
+        echo '<h3 class="my-4">Productos con Unidades &lt;= ' . htmlspecialchars($tope) . '</h3>';
+
 
 
