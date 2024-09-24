ddbab76ec28fb6ce604e2131886aef6cb1255b41
May <cruzmayleth13@gmail.com>
2024-09-24 01:03:28 -0600

    Comprobar conexion

--- practicas/p09-base/set_producto_v2.php
+++ practicas/p09-base/set_producto_v2.php
@@ -5,3 +5,8 @@ $username = "root";
 $password = "";     
 $dbname = "marketzone"; 
 $conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar conexión
+if ($conn->connect_error) {
+    die("Error de conexión: " . $conn->connect_error);
+}
