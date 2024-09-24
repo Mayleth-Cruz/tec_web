5aa9c383842bda8b268679ad63aaf80bec16aa51
May <cruzmayleth13@gmail.com>
2024-09-24 01:02:44 -0600

    Conectar base de datos

---
+++ practicas/p09-base/set_producto_v2.php
@@ -0,0 +1,7 @@
+<?php
+// Conexión a la base de datos
+$servername = "localhost";
+$username = "root";  
+$password = "";     
+$dbname = "marketzone"; 
+$conn = new mysqli($servername, $username, $password, $dbname);
