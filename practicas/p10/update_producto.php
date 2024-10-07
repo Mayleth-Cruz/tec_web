478d9544973c45007f3566243636d132fdc763f6
May <cruzmayleth13@gmail.com>
2024-10-07 13:38:55 -0600

    Recoleccion de datos

--- practicas/p10/update_producto.php
+++ practicas/p10/update_producto.php
@@ -7,3 +7,16 @@ $dbname = "marketzone";  // Nombre de la base de datos
 
 // Crear la conexión
 $link = mysqli_connect($host, $user, $password, $dbname);
+// Chequea la conexión
+if(!$link) {
+    die("ERROR: No pudo conectarse a la base de datos. " . mysqli_connect_error());
+}
+// Recoger los datos del formulario
+$nombre = $_POST['nombre'];
+$marca = $_POST['marca'];
+$modelo = $_POST['modelo'];
+$precio = $_POST['precio'];
+$detalles = $_POST['detalles'];
+$unidades = $_POST['unidades'];
+$imagen = $_FILES['imagen']['name'];  // Nombre de la imagen
+$productId = $_POST['productId'];  // Para saber si es una actualización
