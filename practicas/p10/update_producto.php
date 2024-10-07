c49490a02823d20aea06e9e8040c82851e6c9cb9
May <cruzmayleth13@gmail.com>
2024-10-07 13:37:58 -0600

    Conexion de la base de datos

---
+++ practicas/p10/update_producto.php
@@ -0,0 +1,9 @@
+<?php
+// Datos de conexión a la base de datos
+$host = "localhost";
+$user = "root";
+$password = "";
+$dbname = "marketzone";  // Nombre de la base de datos
+
+// Crear la conexión
+$link = mysqli_connect($host, $user, $password, $dbname);
