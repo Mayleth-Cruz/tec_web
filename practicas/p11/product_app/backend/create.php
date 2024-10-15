415f61ec1c5fe94c3a3de85c3c9baa6531dfa8c6
May <cruzmayleth13@gmail.com>
2024-10-15 00:25:39 -0600

    Validar datos

--- practicas/p11/product_app/backend/create.php
+++ practicas/p11/product_app/backend/create.php
@@ -3,3 +3,12 @@ include_once __DIR__.'/database.php';
 
 // SE VERIFICA QUE HAYA RECIBIDO UN JSON
 $data = json_decode(file_get_contents("php://input"), true);
+if ($data) {
+    // Validar los datos necesarios
+    $nombre = mysqli_real_escape_string($conexion, $data['nombre']);
+    $marca = mysqli_real_escape_string($conexion, $data['marca']);
+    $modelo = mysqli_real_escape_string($conexion, $data['modelo']);
+    $precio = (float)$data['precio'];
+    $unidades = (int)$data['unidades'];
+    $detalles = mysqli_real_escape_string($conexion, $data['detalles']);
+    $imagen = mysqli_real_escape_string($conexion, $data['imagen']);
