3af39ff959b6273050ae4643dfd72de483ca718e
May <cruzmayleth13@gmail.com>
2024-10-22 17:40:55 -0600

    Validaciones

--- practicas/p12/p09_con_jquery/product_app/app.js
+++ practicas/p12/p09_con_jquery/product_app/app.js
@@ -74,4 +74,43 @@ $("#product-form").submit(function (e) {
       unidades: unidades,
       imagen: imagen,
   };
+  
+  // Validaciones de los campos del producto
+  finalproducto['nombre'] = document.getElementById('name').value;
+  if (!finalproducto['nombre'] || finalproducto['nombre'].trim() === '' || finalproducto['nombre'].length > 100) {
+      errores.push('El nombre es obligatorio y debe tener 100 caracteres o menos.');
+  }
+
+  if (isNaN(finalproducto.precio) || finalproducto.precio <= 99.99) {
+      errores.push('El precio debe ser mayor a 99.99.');
+  }
+
+  if (isNaN(finalproducto.unidades) || finalproducto.unidades < 0) {
+      errores.push('Las unidades son obligatorias.');
+  }
+
+  let modelPattern = /^[a-zA-Z0-9]+$/;
+  if (!finalproducto.modelo || finalproducto.modelo.trim() === '' || !modelPattern.test(finalproducto.modelo) || finalproducto.modelo.length > 25) {
+      errores.push('El modelo del producto, debe ser alfanumérico y tener 25 caracteres o menos.');
+  }
+
+  if (!finalproducto.marca || finalproducto.marca.trim() === '') {
+      errores.push('Se debe asignar la marca del producto.');
+  }
+
+  if (finalproducto.detalles && finalproducto.detalles.length > 250) {
+      errores.push('Los detalles no pueden tener más de 250 caracteres.');
+  }
+
+  let defaultImagen = "img/default.jpeg";
+  if (!finalproducto.imagen || finalproducto.imagen.trim() === '') {
+      finalproducto.imagen = defaultImagen;
+  }
+
+  // Si hay errores, mostramos una alerta y no se envía el formulario
+  if (errores.length > 0) {
+      alert(errores.join('\n'));
+      return;
+  }
+
 
