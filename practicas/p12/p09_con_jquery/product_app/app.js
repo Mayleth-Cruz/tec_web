d1f8f2f685ce07efad9081ad632cceebfc41273d
May <cruzmayleth13@gmail.com>
2024-10-22 17:37:39 -0600

    Json base y funcion init

---
+++ practicas/p12/p09_con_jquery/product_app/app.js
@@ -0,0 +1,14 @@
+// JSON BASE A MOSTRAR EN FORMULARIO
+var baseJSON = {
+  precio: 0.0,
+  unidades: 1,
+  modelo: "XX-000",
+  marca: "NA",
+  detalles: "NA",
+  imagen: "img/default.png",
+};
+
+function init() {
+  var JsonString = JSON.stringify(baseJSON, null, 2);
+  document.getElementById("description").value = JsonString;
+}
