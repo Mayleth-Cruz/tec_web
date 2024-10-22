d667ffbc6d08f21be2b15ffbab5965085c07035c
May <cruzmayleth13@gmail.com>
2024-10-22 17:39:11 -0600

    Funcion para buscar un producto

--- practicas/p12/p09_con_jquery/product_app/app.js
+++ practicas/p12/p09_con_jquery/product_app/app.js
@@ -12,3 +12,40 @@ function init() {
   var JsonString = JSON.stringify(baseJSON, null, 2);
   document.getElementById("description").value = JsonString;
 }
+  
+$(document).ready(function () {
+  let edit = false;
+
+  ListaProductos();
+  //Función para buscar productos
+  $("#search").keyup(function (e) {
+    let search = $("#search").val();
+    $.ajax({
+      url: "backend/product-search.php",
+      type: "POST",
+      data: { search },
+      success: function (response) {
+        console.log(response);
+        let productos = JSON.parse(response);
+        let template = "";
+
+        productos.forEach((producto) => {
+          template += `<li>
+                 <li> Nombre: ${producto.nombre}</li>
+                 <li> Precio: ${producto.precio}</li>
+                 <li> Unidades: ${producto.unidades} </li>
+                 <li> Modelo: ${producto.modelo} </li>
+                 <li> Marca: ${producto.marca} </li>
+                 <li>Detalles: ${producto.detalles}</li>
+               
+                    </li>`;
+        });
+
+        if (productos.length > 0) {
+          $("#product-result").removeClass("d-none");
+        }
+
+        $("#container").html(template);
+      },
+    });
+  });
