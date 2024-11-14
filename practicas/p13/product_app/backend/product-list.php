c5d1a403eb93a52768ceb61466e64ee3850d41d4
May <cruzmayleth13@gmail.com>
2024-11-13 21:30:07 -0600

    Php para listar

---
+++ practicas/p13/product_app/backend/product-list.php
@@ -0,0 +1,8 @@
+<?php
+    use api\Products as Products;
+    require_once __DIR__.'/myapi/Products.php';
+
+    $productos = new Products('marketzone');
+    $productos->list();
+    echo $productos->getData();
+?>
